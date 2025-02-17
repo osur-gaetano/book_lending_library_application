7af79d7a7a95736fe87671cfb328581d03944718
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2025-02-17 23:47:25 +0300

    Add validations to book model
    The validation ensure:
      Presence of book title, isbn, author and status.
      Uniqueness of isbn
      Length of a book's isbn is 13 digits in length.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+
+  validates :title, :isbn, :status, :user_id, presence: true
+  validates :isbn, uniqueness: true
+  validates :isb, length: {is: 13, not_standard: "#ISBN should be {count} digits long"}
 end
