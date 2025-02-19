e49b4239db745c90ee49dda99f31440b9fb7f5f0
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2025-02-19 10:01:02 +0300

    Remove direct association between user and book models
    Before a user had direct assoication to books, through user id since
    the first design had put into consideration that an author might be a
    user of our application, which is not correct.
    
    This association will exist when a user has borrowed a book. The
    association will be clear and reasonable when it comes from borrowing.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,16 +1,7 @@
 class Book < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
-=======
-  belongs_to :author, class_name: 'User', foreign_key: 'user_id', dependent: :destroy
->>>>>>> 089412f (Add validations to book model)
-
   validates :title, :isbn, :status, :user_id, presence: true
   validates :isbn, uniqueness: true
-  validates :isb, length: {is: 13, not_standard: "#ISBN should be {count} digits long"}
-<<<<<<< HEAD
-=======
+  validates :isbn, length: {is: 13, not_standard: "#ISBN should be {count} digits long"}
 
-  enum status: {Available: 'Available', Borrowed: "Borrowed"}
->>>>>>> 089412f (Add validations to book model)
+  enum status: {available: 'available', borrowed: "borrowed"}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  has_many :books
-
   validates :username, :email, presence: true
   validates :email, uniqueness: true
 end
