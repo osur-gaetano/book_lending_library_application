a40e169d2f34380d42ffe1b07f3e622221a51fab
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2025-02-19 10:23:00 +0300

    Add validation and Callback to Borrowing model
    The validations ensures that borrowed date and due data are present.
    Also
    The callback ensures that the book borrowed date and due date are set
    appropriately before a borrowing object is created into the database.

--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,4 +1,15 @@
 class Borrowing < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :borrowed_date, :due_date, presence: true
+  before_validation :set_due_date, on: :create
+
+
+  private
+  def set_due_date
+    self.borrowed_date ||= Time.current
+    self.due_date ||= borrowed_date + 2.weeks
+  end
+
 end
