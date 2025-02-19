8f6806481e291d1ade8fde75e6c10d5ad3954ec6
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2025-02-19 10:13:30 +0300

    Add author column to books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_19_070940) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_19_071241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_19_070940) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
     t.index ["isbn"], name: "index_books_on_isbn", unique: true
   end
 
